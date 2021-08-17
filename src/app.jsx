e912a671cca90cbcec91a893bfa0fe3a499c3c6e
m1172 <muhshuh2000@gmail.com>
2021-08-17 11:23:11 +0200

    mock

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {data} from './mock'
 import './App.css';
 
 export default class App extends Component {
@@ -7,10 +8,23 @@ export default class App extends Component {
         this.state = {};
     }
     render() {
-        return (
+        return ( 
             <div>
                 <div className='wrapper'>
-                    <div className="menu">menu</div>
+                    <div className="menu">
+                        {data.map(value=>{
+                            return(
+                                <div className="category">
+                                    <h1>{value.category}</h1>
+                                    <div className="submenu">
+                                        {value.lists.map((item)=>(
+                                            <div className="item">{item.title}</div>
+                                        ))}
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
                     <div className="order">order</div>
                 </div>
             </div>
---
+++ src/mock.js
@@ -0,0 +1,26 @@
+export const data = [
+    { id:1,
+      category: 'Chicken',
+      lists: [
+        {id: 11, title: 'Cripsy Chicken', src:'', price: 10000},
+        {id: 12, title: 'Hot Spicy Chicken', src:'', price: 10000},
+        {id: 13, title: 'BBQ Chicken', src:'', price: 10000},
+        {id: 14, title: 'Chicken', src:'', price: 10000},
+    ]},
+    { id:2,
+      category: 'BBQ',
+      lists: [
+        {id: 21, title: 'BBQ Chicken', src:'', price: 10000},
+        {id: 22, title: 'BBQ Crispy Chicken', src:'', price: 10000},
+        {id: 23, title: 'Hot BBQ Chicken', src:'', price: 10000},
+        {id: 24, title: 'BBQ', src:'', price: 10000},
+    ]},
+    { id:3, 
+      category: 'SET',
+      lists: [
+        {id: 31, title: 'SET Chicken', src:'', price: 10000},
+        {id: 32, title: 'SET BBQ', src:'', price: 10000},
+        {id: 33, title: 'SET A Chicken', src:'', price: 10000},
+        {id: 34, title: 'SET B Chicken', src:'', price: 10000},
+    ]},
+]
