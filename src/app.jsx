1fd696697393d5a66966d44cf25337d166e503a7
m1172 <muhshuh2000@gmail.com>
2021-08-17 14:33:50 +0200

    button

--- src/app.jsx
+++ src/app.jsx
@@ -5,27 +5,57 @@ import './App.css';
 export default class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            selected: [],
+            total: 0,
+        };
     }
     render() {
+        const onSelect =(value) => {
+            const selected = [...this.state.selected, value];
+            this.setState({selected});
+            selected.forEach((value) => 
+                this.setState({total: this.state.total + value.price})
+            );
+        }; 
         return ( 
             <div>
                 <div className='wrapper'>
                     <div className="menu">
                         {data.map(value=>{
                             return(
-                                <div className="category">
-                                    <h1>{value.category}</h1>
+                                <div className="category" > 
+                                    <h1 className='title'>{value.category}</h1>
                                     <div className="submenu">
                                         {value.lists.map((item)=>(
-                                            <div className="item">{item.title}</div>
+                                            <div onClick={() => onSelect(item)} className="item">
+                                                <img src={item.src} alt="" />
+                                                <h1>{item.title}</h1>
+                                                <h2 className='price'>{item.price}som</h2>
+                                            </div>
                                         ))}
-                                    </div>
+                                    </div> 
                                 </div>
                             )
-                        })}
+                        })} 
                     </div>
-                    <div className="order">order</div>
+                    <div className="order">
+                        <h1 className='title'>Order</h1>
+                        {this.state.selected.length <= 0 && (
+                            <h1>Not Selected</h1>
+                        )}
+                        {this.state.selected.map((value) => (
+                            <div className="order">
+                                <h1>
+                                    {value.title} - {value.price}som
+                                    <button onClick={()=> onDelete()}>onDelete()</button>
+                                </h1>
+                            </div>
+                        ))}
+                        <h1 className="title">Total {this.state.total} som</h1>
+                        <button onClick={() => this.setState({selected: [], total: 0})}>Cancel</button>
+                        <button>Order</button>
+                    </div>
                 </div>
             </div>
         );
